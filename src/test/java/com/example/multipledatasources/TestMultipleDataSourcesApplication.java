45c6f2d33dcc0d8c859b63c2c7807721004517c8
Raja Kolli <rajadilipkolli@gmail.com>
2023-10-27 09:37:20 +0000

    fixes spotless format issue

--- src/test/java/com/example/multipledatasources/TestMultipleDataSourcesApplication.java
+++ src/test/java/com/example/multipledatasources/TestMultipleDataSourcesApplication.java
@@ -2,7 +2,6 @@ package com.example.multipledatasources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
