c39481a53ab576667963f3250b9fd45242acdeb3
Raja Kolli <rajadilipkolli@gmail.com>
2023-10-27 09:24:45 +0000

    increment google format version

--- src/main/java/com/example/multipledatasources/dto/ResponseDto.java
+++ src/main/java/com/example/multipledatasources/dto/ResponseDto.java
@@ -1,6 +1,3 @@
 package com.example.multipledatasources.dto;
 
-import java.io.Serializable;
-
-public record ResponseDto(String memberId, String cardNumber, String memberName)
-        implements Serializable {}
+public record ResponseDto(String memberId, String cardNumber, String memberName) {}
