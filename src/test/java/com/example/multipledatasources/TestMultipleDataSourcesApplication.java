a90a57c535ae537baf1fa5a8c853a9c700d6ce14
Raja Kolli <rajadilipkolli@gmail.com>
2024-01-06 11:07:46 +0000

    attempt to fix

--- src/test/java/com/example/multipledatasources/TestMultipleDataSourcesApplication.java
+++ src/test/java/com/example/multipledatasources/TestMultipleDataSourcesApplication.java
@@ -2,7 +2,6 @@ package com.example.multipledatasources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.MySQLContainer;
@@ -11,29 +10,33 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @TestConfiguration(proxyBeanMethods = false)
 public class TestMultipleDataSourcesApplication {
 
-    @ServiceConnection
     @Bean
     // @DependsOn("mySQLContainer")
     PostgreSQLContainer<?> postgreSQLContainer(DynamicPropertyRegistry dynamicPropertyRegistry) {
         PostgreSQLContainer<?> postgreSQLContainer =
-                new PostgreSQLContainer<>("postgres:16.0-alpine");
+                new PostgreSQLContainer<>("postgres:16.1-alpine").withDatabaseName("memberdb");
         dynamicPropertyRegistry.add("app.datasource.member.url", postgreSQLContainer::getJdbcUrl);
         dynamicPropertyRegistry.add(
                 "app.datasource.member.username", postgreSQLContainer::getUsername);
         dynamicPropertyRegistry.add(
                 "app.datasource.member.password", postgreSQLContainer::getPassword);
+        dynamicPropertyRegistry.add("spring.liquibase.url", postgreSQLContainer::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.liquibase.user", postgreSQLContainer::getUsername);
+        dynamicPropertyRegistry.add("spring.liquibase.password", postgreSQLContainer::getPassword);
         return postgreSQLContainer;
     }
 
-    @ServiceConnection
     @Bean
     MySQLContainer<?> mySQLContainer(DynamicPropertyRegistry dynamicPropertyRegistry) {
-        MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.1");
+        MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.2");
         dynamicPropertyRegistry.add("app.datasource.cardholder.url", mySQLContainer::getJdbcUrl);
         dynamicPropertyRegistry.add(
                 "app.datasource.cardholder.username", mySQLContainer::getUsername);
         dynamicPropertyRegistry.add(
                 "app.datasource.cardholder.password", mySQLContainer::getPassword);
+        dynamicPropertyRegistry.add("spring.flyway.url", mySQLContainer::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.flyway.user", mySQLContainer::getUsername);
+        dynamicPropertyRegistry.add("spring.flyway.password", mySQLContainer::getPassword);
         return mySQLContainer;
     }
 
