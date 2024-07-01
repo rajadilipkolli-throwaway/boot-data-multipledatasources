3d0c59b53634c2b5935f34c7d2602292c20d6ef5
Raja Kolli <rajadilipkolli@gmail.com>
2024-07-01 14:40:20 +0000

    use static way to load at start

--- src/test/java/com/example/multipledatasources/TestMultipleDataSourcesApplication.java
+++ src/test/java/com/example/multipledatasources/TestMultipleDataSourcesApplication.java
@@ -1,39 +1,38 @@
 package com.example.multipledatasources;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.lifecycle.Startables;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestMultipleDataSourcesApplication {
 
-    @Autowired private DynamicPropertyRegistry dynamicPropertyRegistry;
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.4");
 
-    @Bean
-    MySQLContainer<?> mySQLContainer() {
-        MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.4");
+    static PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:16.3-alpine");
+
+    static {
+        Startables.deepStart(mySQLContainer, postgreSQLContainer).join();
+    }
+
+    @DynamicPropertySource
+    void addApplicationProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+
         dynamicPropertyRegistry.add("app.datasource.cardholder.url", mySQLContainer::getJdbcUrl);
         dynamicPropertyRegistry.add(
                 "app.datasource.cardholder.username", mySQLContainer::getUsername);
         dynamicPropertyRegistry.add(
                 "app.datasource.cardholder.password", mySQLContainer::getPassword);
-        return mySQLContainer;
-    }
-
-    @Bean
-    PostgreSQLContainer<?> postgreSQLContainer() {
-        PostgreSQLContainer<?> postgreSQLContainer =
-                new PostgreSQLContainer<>("postgres:16.2-alpine");
         dynamicPropertyRegistry.add("app.datasource.member.url", postgreSQLContainer::getJdbcUrl);
         dynamicPropertyRegistry.add(
                 "app.datasource.member.username", postgreSQLContainer::getUsername);
         dynamicPropertyRegistry.add(
                 "app.datasource.member.password", postgreSQLContainer::getPassword);
-        return postgreSQLContainer;
     }
 
     public static void main(String[] args) {
